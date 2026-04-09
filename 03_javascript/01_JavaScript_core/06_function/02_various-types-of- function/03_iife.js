2b833fddd61dcd2ddea9ca59a60d665375c4b68a
dmstj0162 <dmstj0162@gmail.com>
2026-04-09 14:16:54 +0900

    즉시 실행 함수 정리

---
+++ 03_javascript/01_JavaScript_core/06_function/02_various-types-of- function/03_iife.js
@@ -0,0 +1,23 @@
+/**
+ * 즉시 실행 함수(IIFE)
+ * 함수를 정의하자마자 딱 한 번만 실행되는 '일회용 함수'이다.
+ */
+
+(function () {
+  let secretMessage = "이 변수는 이 안에서만 존재한다."; //함수 안에서만 존재
+  console.log("즉시 실행 함수가 실행되었습니다.");
+  console.log(secretMessage);
+})();
+
+// (function() {}) ();
+
+// console.log(secretMessage);
+
+const result = (function (name) {
+  let a = 10;
+  let b = 20;
+  console.log(name);
+  return a + b;
+})("은서");
+
+console.log(result);
