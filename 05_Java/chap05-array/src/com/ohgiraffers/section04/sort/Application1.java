78eb83635d96ce4a1aa55e8fd402dd5ece90e13e
dmstj0162 <dmstj0162@gmail.com>
2026-05-01 14:40:39 +0900

    sort

---
+++ 05_Java/chap05-array/src/com/ohgiraffers/section04/sort/Application1.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section04.sort;
+
+public class Application1 {
+    public static void main(String[] args) {
+
+        /* 변수의 두 값을 변경하는 방법*/
+        int num1 = 10;
+        int num2 = 20;
+
+        int temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("num1 : " + num1 + "num2 : " + num2);
+
+        int[] arr = {2,1,3};
+
+        int temp2 = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp2;
+
+    }
+}
