6d8ef9cf600c7cbade8e105990c06f0a9e56680c
dmstj0162 <dmstj0162@gmail.com>
2026-05-04 12:10:17 +0900

    오버로딩

---
+++ 05_Java/chap06-class-and-object/src/com/ohgiraffers/section05/overloading/OverloadingRules.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section05.overloading;
+
+public class OverloadingRules {
+    /*
+    * [메서드 시그니처]
+    * 메서드를 식별하는 고유한 바코드 = '메서드 이름' + '매개변수 타입, 개수, 순서'
+    *
+    * 오버로딩: 이름은 같지만 시그니처가 다른 메서드를 여러 개 정의하는 기술
+    * */
+
+    public void test(){
+
+    }
+}
