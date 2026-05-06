7403ceb470e55e05e628e2827855f508d6b06999
dmstj0162 <dmstj0162@gmail.com>
2026-05-06 17:41:24 +0900

    객체배열 연습 문제

---
+++ 05_Java/assignment/src/com/greedy/level02/normal/book/model/dto/StudentDTO.java
@@ -0,0 +1,31 @@
+package com.greedy.level02.normal.book.model.dto;
+
+public class StudentDTO {
+    private int grade;
+    private int classroom;
+    private String name;
+    private int kor;
+    private int eng;
+    private int math;
+
+    // 생성자
+    public StudentDTO() {}
+
+    public StudentDTO(int grade, int classroom, String name, int kor, int eng, int math){
+        this.grade = grade;
+        this.classroom = classroom;
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
+    public String getInformation() {
+        return "학년 : " + grade +
+                ", 반 : " + classroom +
+                ", 이름 : " + name +
+                ", 국어점수 : " + kor +
+                ", 영어점수 : " + eng +
+                ", 수학점수 : " + math;
+    }
+}
---
+++ 05_Java/assignment/src/com/greedy/level02/normal/book/run/Application1.java
@@ -0,0 +1,9 @@
+package com.greedy.level02.normal.book.run;
+
+import com.greedy.level02.normal.book.model.dto.StudentDTO;
+
+public class Application1 {
+    public static void main(String[] args) {
+        StudentDTO dto = new StudentDTO();
+    }
+}
