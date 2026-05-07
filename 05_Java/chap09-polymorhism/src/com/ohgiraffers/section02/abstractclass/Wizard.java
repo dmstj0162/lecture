6dacb4a0ff11e437bb0b29a07f9884723a3e354b
dmstj0162 <dmstj0162@gmail.com>
2026-05-07 14:45:19 +0900

    abstract class

---
+++ 05_Java/chap09-polymorhism/src/com/ohgiraffers/section02/abstractclass/Wizard.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.abstractclass;
+
+public class Wizard extends Player {
+
+    @Override
+    public void attack() {
+        System.out.println("마법사가 지팡이로 공격합니다.");
+    }
+}
