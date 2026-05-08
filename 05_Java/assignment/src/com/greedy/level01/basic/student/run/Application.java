f3ba4af500e9c0b1476ab1521c5a84d37f9a1701
dmstj0162 <dmstj0162@gmail.com>
2026-05-08 09:15:28 +0900

    자바 연산 과제

---
+++ 05_Java/assignment/src/com/greedy/level01/basic/student/run/Application.java
@@ -0,0 +1,46 @@
+package com.greedy.level01.basic.student.run;
+
+import com.greedy.level02.normal.book.model.dto.StudentDTO;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        // 객체 배열 선언
+//        StudentDTO[] students = new StudentDTO[10];
+//        int count = 0;
+//
+//        // while
+//        while(count < 10){
+//            System.out.print("학년 :");
+//            int grade = sc.nextInt();
+//            System.out.print("반 :");
+//            int classroom = sc.nextInt();
+//            System.out.print("이름 :");
+//            sc.nextLine();
+//            String name = sc.nextLine();
+//            System.out.print("국어점수 :");
+//            int kor = sc.nextInt();
+//            System.out.print("영어점수 :");
+//            int eng = sc.nextInt();
+//            System.out.print("수학점수 :");
+//            int math = sc.nextInt();
+//
+//            StudentDTO student = new StudentDTO();
+//            student.setGrade(grade);
+//            student.setClassroom(classroom);
+//            student.setName(name);
+//            student.setKor(kor);
+//            student.setEng(eng);
+//            student.setMath(math);
+//
+//            students[count] = student;
+//            count++;
+//
+//        }
+
+
+    }
+}
