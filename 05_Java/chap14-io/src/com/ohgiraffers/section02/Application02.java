7bf0058269ba6ce0f8e5d111575d0105a02ceb9b
dmstj0162 <dmstj0162@gmail.com>
2026-05-12 14:16:51 +0900

    buffer

---
+++ 05_Java/chap14-io/src/com/ohgiraffers/section02/Application02.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.section02;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Application02 {
+    public static void main(String[] args) {
+
+    /* FileOutputStream : 데이터를 1바이트씩 파일에 출력하는 스트림*/
+
+        // try-with-resources : try() 소괄호 안에서 자원을 선언하면
+        // try 블럭이 끝날때(정상이든 예외든) 자동으로 close() 호출
+
+        try(FileOutputStream fout = new FileOutputStream("src/")){
+
+            fout.write(97);     // 아스키코드 97 -> a
+
+            byte[] bar = new byte[] {98, 99, 100, 101, 102, 10};
+            // 배열 일부만 쓰기
+            // write(byte[], offset, length)
+
+            fout.write(bar);
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        // try 블록 끝 -> 자동으로 fout.close() 호출됨
+    }
+
+}
