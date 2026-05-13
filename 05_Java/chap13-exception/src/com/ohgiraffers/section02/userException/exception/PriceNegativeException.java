fe094aaf958773f482751d4ba11b3f12f4540e88
dmstj0162 <dmstj0162@gmail.com>
2026-05-13 22:18:32 +0900

    커스텀 예외처리

---
+++ 05_Java/chap13-exception/src/com/ohgiraffers/section02/userException/exception/PriceNegativeException.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.userException.exception;
+
+public class PriceNegativeException extends NegativeException{
+
+    public PriceNegativeException(String message) {
+        super(message);
+    }
+}
