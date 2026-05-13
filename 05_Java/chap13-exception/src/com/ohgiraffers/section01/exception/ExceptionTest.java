6f58c840b645f76f52ba5874023d6154d50da44a
dmstj0162 <dmstj0162@gmail.com>
2026-05-13 21:48:44 +0900

    예외처리

---
+++ 05_Java/chap13-exception/src/com/ohgiraffers/section01/exception/ExceptionTest.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.exception;
+
+public class ExceptionTest {
+
+    // throws: 에러를 메소드를 호출한 쪽으로 위임
+    public void checkEnoughMoney(int price, int money) throws Exception{
+        System.out.println("가지고 계신 돈은 " + money +"원 입니다.");
+
+        if(money >= price){
+            System.out.println("상품 구입하기 위한 돈 충분합니다.");
+        }else{
+            throw new Exception("돈이 부족합니다.");   // 에러 발생
+        }
+
+        System.out.println("즐거운 쇼핑하세용");
+    }
+}
