000853fe5e468e3ad0df36b304bea575fe463740
dmstj0162 <dmstj0162@gmail.com>
2026-06-10 14:34:52 +0900

    JPA

---
+++ 11_jpa/chap01_persistence-context/src/test/java/com/ohgiraffers/section03/entity/EntityLifeCycleTests.java
@@ -0,0 +1,184 @@
+package com.ohgiraffers.section03.entity;
+
+import com.ohgiraffers.section02.crud.EntityManagerGenerator;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EntityLifeCycleTests {
+
+    private EntityLifeCycle lifeCycle;
+
+    @BeforeEach
+    void setup() {
+        // 테스트 시작할 때마다 새로 만들어짐
+        this.lifeCycle = new EntityLifeCycle();
+    }
+
+    @DisplayName("비영속 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {2})
+    void testTransient(int menuCode) {
+        // when
+        Menu foundMenu = lifeCycle.findMenuByMenuCode(menuCode);
+
+        Menu newMenu = new Menu(
+                foundMenu.getMenuCode(),
+                foundMenu.getMenuName(),
+                foundMenu.getMenuPrice(),
+                foundMenu.getCategoryCode(),
+                foundMenu.getOrderableStatus()
+        );
+
+        EntityManager entityManager = lifeCycle.getManagerInstance();
+
+        // then
+        assertNotEquals(foundMenu, newMenu);
+        assertTrue(entityManager.contains(foundMenu));
+        assertFalse(entityManager.contains(newMenu));
+    }
+
+    @DisplayName("다른 엔티티 매니저가 관리하는 엔티티의 영속성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3})
+    void testManagedOtherEntityManager(int menuCode) {
+        // when
+        Menu menu1 = lifeCycle.findMenuByMenuCode(menuCode);
+        Menu menu2 = lifeCycle.findMenuByMenuCode(menuCode);
+
+        //then
+        assertNotEquals(menu1, menu2);
+    }
+
+    @DisplayName("같은 엔티티 매니저가 관리하는 엔티티의 영속성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {3})
+    void testManagedSameEntityManager(int menuCode) {
+        //given
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+
+        // when
+        Menu menu1 = entityManager.find(Menu.class, menuCode);
+        Menu menu2 = entityManager.find(Menu.class, menuCode);
+
+        //then
+        assertEquals(menu1, menu2);
+    }
+
+    @DisplayName("준영속화 detach 테스트")
+    @ParameterizedTest
+    @CsvSource("11, 1000")
+    void testDetachEntity(int menuCode, int menuPrice){
+        // given
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        // when
+        entityTransaction.begin();
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+        // detach: 특정 엔티티만 준영속 상태(영속성 컨텍스트가 관리하던 개게를 관리하지 않는 상태)로 만든다.
+        entityManager.detach(foundMenu);
+        foundMenu.setMenuPrice(menuPrice);
+        // flush : 영속성 컨텍스트의 상태를 DB에 내보낸다. commit 하지 않은 상태이므로 rollback 가능
+        entityManager.flush();
+
+        //then
+        assertNotEquals(menuPrice, entityManager.find(Menu.class, menuCode).getMenuPrice());
+        entityTransaction.rollback();
+    }
+
+    @DisplayName("준영속화 detach 후 다시 영속화 테스트")
+    @ParameterizedTest
+    @CsvSource("11, 1000")
+    void testDetachAndMerge(int menuCode, int menuPrice){
+        // given
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        // when
+        entityTransaction.begin();
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+        // detach: 특정 엔티티만 준영속 상태(영속성 컨텍스트가 관리하던 개게를 관리하지 않는 상태)로 만든다.
+        entityManager.detach(foundMenu);
+        foundMenu.setMenuPrice(menuPrice);
+
+        entityManager.merge(foundMenu);
+        entityManager.flush();
+
+        //then
+        assertEquals(menuPrice, entityManager.find(Menu.class, menuCode).getMenuPrice());
+        entityTransaction.rollback();
+    }
+
+    @DisplayName("detach후 merge한 데이터 update 테스트")
+    @ParameterizedTest
+    @CsvSource("11, 전복죽")
+    void testDetachAndMerge(int menuCode, String menuName) {
+
+        //given
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+        entityManager.detach(foundMenu);
+
+        //when
+        foundMenu.setMenuName(menuName);
+        Menu refoundMenu = entityManager.find(Menu.class, menuCode);
+
+        entityManager.merge(foundMenu);
+
+        //then
+        assertEquals(menuName, refoundMenu.getMenuName());
+    }
+
+    @DisplayName("준영속화 close 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {6})
+    void testClosePersistenceContext(int menuCode) {
+        //given
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+
+        //when
+        //close: 컨텍스트를 종료한다.
+        entityManager.close();
+
+        //then
+        assertThrows(
+                IllegalStateException.class,
+                () -> entityManager.find(Menu.class, menuCode)
+        );
+    }
+
+    @DisplayName("엔티티 remove 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {6})
+    void testRemoveEntity(int menuCode) {
+        //given
+        EntityManager entityManager = EntityManagerGenerator.getInstance();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        Menu foundMenu = entityManager.find(Menu.class, menuCode);
+
+        //when
+        entityTransaction.begin();
+        //remove: 엔티티를 영속성 컨텍스트 및 데이터베이스에서 삭제
+        // 단, 트랜잭션을 제어하지 않으면 DB에 영구 반영되지는 않음
+        // 커밋하는 순간 영속성 컨텍스트에서 관리하는 엔티티 객체가 데이터베이스에 반영된다.
+        entityManager.remove(foundMenu);
+
+        entityManager.flush();      // 영속성 컨텍스트 내용을 DB에 동기화하는 작업
+
+        //then
+        Menu refoundMenu = entityManager.find(Menu.class, menuCode);
+        assertNull(refoundMenu);
+        entityTransaction.rollback();
+    }
+
+
+
+}
