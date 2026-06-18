dd6663f28ddf3df4f441ed628ed92dbf3cc23d8a
dmstj0162 <dmstj0162@gmail.com>
2026-06-18 11:33:46 +0900

    jpa 끝

--- 11_jpa/chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/main/controller/MainController.java
+++ 11_jpa/chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/main/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.ohgiraffers.springdatajpa.main.controller;
 
+import com.ohgiraffers.springdatajpa.menu.dto.MenuRequestDTO;
+import com.ohgiraffers.springdatajpa.menu.dto.MenuResponseDTO;
+import com.ohgiraffers.springdatajpa.menu.service.MenuService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -9,8 +15,16 @@ import java.util.Map;
 @RestController
 public class MainController {
 
+    private final MenuService menuService;
+
+    public MainController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
     @GetMapping("/")
     public ResponseEntity<Map<String, String>> main() {
         return ResponseEntity.ok(Map.of("message", "메인 API 페이지에 오신 것을 환영합니다."));
     }
+
+
 }
--- 11_jpa/chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ 11_jpa/chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -1,6 +1,9 @@
 package com.ohgiraffers.springdatajpa.menu.service;
 
+import com.ohgiraffers.springdatajpa.menu.dto.CategoryDTO;
+import com.ohgiraffers.springdatajpa.menu.dto.MenuRequestDTO;
 import com.ohgiraffers.springdatajpa.menu.dto.MenuResponseDTO;
+import com.ohgiraffers.springdatajpa.menu.entity.Category;
 import com.ohgiraffers.springdatajpa.menu.entity.Menu;
 import com.ohgiraffers.springdatajpa.menu.repository.CategoryRepository;
 import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
@@ -8,7 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -54,5 +61,65 @@ public class MenuService {
         Page<Menu> menuList = menuRepository.findByMenuPriceGreaterThan(menuPrice, pageable);
 
         return menuList.map(menu -> modelMapper.map(menu, MenuResponseDTO.class));
+    }
+
+    /* 카테고리 조회 */
+    public List<CategoryDTO> findAllCategory() {
+        List<Category> categoryList = categoryRepository.findAllCategory();
+        return categoryList.stream()
+                .map(category -> modelMapper.map(category, CategoryDTO.class))
+                .toList();
+    }
+
+    /* 메뉴 등록 */
+    @Transactional
+    public MenuResponseDTO registMenu(MenuRequestDTO requestDTO) {
+
+        Category category = categoryRepository.findById(requestDTO.getCategoryCode())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카테고리입니다."));
+
+        // DTO -> Entity 변환(builder 패턴 사용)
+        Menu newMenu = Menu.builder()
+                .menuName(requestDTO.getMenuName())
+                .orderableStatus(requestDTO.getOrderableStatus())
+                .menuPrice(requestDTO.getMenuPrice())
+                .category(category)
+                .build();
+
+        // 내부적으로 EntityManager.persist() 호출되어 영속성 컨텍스트로 들어간다.
+        Menu savedMenu = menuRepository.save(newMenu);
+
+        // 저장 후, 생성된 Entity를 다시 DTO로 변환하여 반환
+        return modelMapper.map(savedMenu, MenuResponseDTO.class);
+    }
+
+    /* 메뉴 수정 */
+    @Transactional
+    public MenuResponseDTO modifyMenu(int menuCode, MenuRequestDTO requestDTO){
+
+        Menu foundMenu = menuRepository.findById(menuCode)
+                .orElseThrow(() -> new IllegalArgumentException("수정할 메뉴가 존재하지 않습니다."));
+
+        // 변경할 카테고리 조회
+        Category newCategory = categoryRepository.findById(requestDTO.getCategoryCode())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카테고리입니다."));
+
+        foundMenu.modify(
+                requestDTO.getMenuName(),
+                requestDTO.getMenuPrice(),
+                newCategory,
+                requestDTO.getOrderableStatus()
+        );
+        return modelMapper.map(foundMenu, MenuResponseDTO.class);
+    }
+
+    /* 메뉴 삭제*/
+    @Transactional
+    public void deleteMenu(int menuCode) {
+
+        if(!menuRepository.existsById(menuCode)){
+            throw new IllegalArgumentException("삭제할 메뉴가 존재하지 않습니다.");
+        }
+        menuRepository.deleteById(menuCode);
     }
 }
