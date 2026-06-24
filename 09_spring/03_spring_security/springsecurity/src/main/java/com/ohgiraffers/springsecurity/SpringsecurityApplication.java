8953724b15dea587a81a7214b2d077d3e7099bbc
dmstj0162 <dmstj0162@gmail.com>
2026-06-24 11:04:24 +0900

    feat : Security 0624

--- 09_spring/03_spring_security/springsecurity/src/main/java/com/ohgiraffers/springsecurity/SpringsecurityApplication.java
+++ 09_spring/03_spring_security/springsecurity/src/main/java/com/ohgiraffers/springsecurity/SpringsecurityApplication.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.springsecurity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ohgiraffers.springsecurity.query.mapper")
 public class SpringsecurityApplication {
 
     public static void main(String[] args) {
