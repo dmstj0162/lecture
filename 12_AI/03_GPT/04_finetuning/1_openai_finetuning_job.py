8c079b222f79c1e5a527a11055b16e1f1fdf787d
dmstj0162 <dmstj0162@gmail.com>
2026-07-20 10:59:51 +0900

    addfinetuning examples

---
+++ 12_AI/03_GPT/04_finetuning/1_openai_finetuning_job.py
@@ -0,0 +1,26 @@
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+client=OpenAI()
+BASE_DIR = Path(__file__).resolve().parent
+print(BASE_DIR)
+dataset = BASE_DIR / "fine_tuning_dataset_example.jsonl"
+
+with dataset.open("rb") as f:
+    uploaded = client.files.create(
+        file=f,
+        purpose="fine-tune"
+    )
+print("=== 업로드 완료===")
+print("File ID: ", uploaded.id)
+
+try:
+    job = client.fine_tuning.jobs.create(
+        training_file=uploaded.id,
+        moel="gpt-4.1-mini"
+    )
+    print("job.id: ", job.id)
+except Exception as e:
+    print(e)
